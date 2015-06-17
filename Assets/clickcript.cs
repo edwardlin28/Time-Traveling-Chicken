c2366aa7511d7ef58a0b1a524204e05301e8c0a0
Guowei Lin <edward.lin.28@gmail.com>
2015-06-16 19:55:33 -0700

    midterm

---
+++ Assets/clickcript.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class clickcript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (GUI.Button(new Rect(15,15,200,200),GUIStyle.none))
+		    {
+			Application.LoadLevel(1);
+
+		}
+	}
+}
